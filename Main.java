29d607b63e207c88189fb480a4ea7e504f7627a6
Rachel Guzman Valencio <rachelguzmanv@yahoo.com>
2024-07-24 10:53:33 -0300

    Forçando a remoção do repositório aninhado meu-primeiro-programa-udemy

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args){
+
+		JFrame janela = new JFrame();
+		janela.setTitle("Hello world!");
+		janela.setSize(500, 500);
+		janela.setVisible(true);
+
+	}
+}
